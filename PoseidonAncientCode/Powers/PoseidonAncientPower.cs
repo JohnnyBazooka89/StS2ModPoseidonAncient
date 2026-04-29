04dd86465145f7e5cbbc55b977903dc276fa8914
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-04-29 20:48:45 +0200

    Add Content Mod from template.

---
+++ PoseidonAncientCode/Powers/PoseidonAncientPower.cs
@@ -0,0 +1,13 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using PoseidonAncient.PoseidonAncientCode.Extensions;
+using Godot;
+
+namespace PoseidonAncient.PoseidonAncientCode.Powers;
+
+public abstract class PoseidonAncientPower : CustomPowerModel
+{
+    //Loads from PoseidonAncient/images/powers/your_power.png
+    public override string CustomPackedIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PowerImagePath();
+    public override string CustomBigIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigPowerImagePath();
+}
---
+++ PoseidonAncientCode/Relics/PoseidonAncientRelic.cs
@@ -0,0 +1,15 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using PoseidonAncient.PoseidonAncientCode.Extensions;
+using Godot;
+
+namespace PoseidonAncient.PoseidonAncientCode.Relics;
+
+public abstract class PoseidonAncientRelic : CustomRelicModel
+{
+    //PoseidonAncient/images/relics
+    public override string PackedIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".RelicImagePath();
+    protected override string PackedIconOutlinePath =>
+        $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_outline.png".RelicImagePath();
+    protected override string BigIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigRelicImagePath();
+}
