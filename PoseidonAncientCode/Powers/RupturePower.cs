29604f0414a46c8e79235e561b784360fcb41bfa
JohnnyBazooka89 <johnnybazooka89@gmail.com>
2026-05-04 20:07:14 +0200

    Added HP Bar Forecast on Rupture Power.

--- PoseidonAncientCode/Powers/RupturePower.cs
+++ PoseidonAncientCode/Powers/RupturePower.cs
@@ -1,3 +1,5 @@
+using BaseLib.Hooks;
+using Godot;
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Powers;
@@ -8,6 +10,8 @@ namespace PoseidonAncient.PoseidonAncientCode.Powers;
 
 public class RupturePower : PoseidonAncientPower
 {
+    private static readonly Color Color = new(110/255f, 0, 0);
+
     public override PowerType Type => PowerType.Debuff;
 
     public override PowerStackType StackType => PowerStackType.Counter;
@@ -20,5 +24,10 @@ public class RupturePower : PoseidonAncientPower
         }
 
         await CreatureCmd.Damage(new ThrowingPlayerChoiceContext(), Owner, Amount, ValueProp.Unpowered, Owner);
+    }
+
+    public override IEnumerable<HealthBarForecastSegment> GetHealthBarForecastSegments(HealthBarForecastContext context)
+    {
+        return [new HealthBarForecastSegment(Amount, Color, HealthBarForecastDirection.FromRight)];
     }
 }
